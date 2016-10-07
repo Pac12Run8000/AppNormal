870646a7aa28391febca13c15760403fff108f09
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2016-10-06 21:34:16 -0700

    version 00.05

--- AppNormal/LoginController+handlers.swift
+++ AppNormal/LoginController+handlers.swift
@@ -28,8 +28,8 @@ extension LoginController: UIImagePickerControllerDelegate, UINavigationControll
             }
             
            
-            
-            let storageRef = FIRStorage.storage().reference().child("myImage.png")
+            let imageName = NSUUID().UUIDString
+            let storageRef = FIRStorage.storage().reference().child("profile_images").child("\(imageName).png")
             
             if let uploadData = UIImagePNGRepresentation(self.profileImage.image!) {
                 storageRef.putData(uploadData, metadata: nil, completion: { (metaData, error) in
@@ -37,23 +37,28 @@ extension LoginController: UIImagePickerControllerDelegate, UINavigationControll
                         print("Upload Image ERROR:", error)
                         return
                     }
-                    print(metaData)
+                    if let profileImageUrl = metaData?.downloadURL()?.absoluteString {
+                        let values = ["name": name, "email": email, "profileImageUrl": profileImageUrl]
+                        self.registerUserIntoDatabaseWithUID(uid, values: values)
+                    }
+                    
+                    
                 })
             }
             
-            
-            let ref = FIRDatabase.database().referenceFromURL("https://appnormal-e8c55.firebaseio.com/")
-            let usersReference = ref.child("users").child(uid)
-            let values = ["name": name, "email": email]
-            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
-                if (err != nil) {
-                    print(err)
-                    return
-                }
-                self.dismissViewControllerAnimated(true, completion: nil)
-                print("Saved successfully")
-            })
-            
+        })
+    }
+    
+    private func registerUserIntoDatabaseWithUID(uid:String, values: [String: AnyObject]) {
+        let ref = FIRDatabase.database().referenceFromURL("https://appnormal-e8c55.firebaseio.com/")
+        let usersReference = ref.child("users").child(uid)
+        usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+            if (err != nil) {
+                print(err)
+                return
+            }
+            self.dismissViewControllerAnimated(true, completion: nil)
+            print("Saved successfully")
         })
     }
     
