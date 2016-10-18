cb69a67c527c7c486ad361700691fe5ead863c83
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2016-10-18 15:56:16 -0700

    version 00.21

--- AppNormal/FeedViewController.swift
+++ AppNormal/FeedViewController.swift
@@ -7,26 +7,72 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UITableViewController {
+    
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationItemsAndBarButtons()
+        checkIfUserLoggedInAndSetUpNavbarTitle()
+        
+        tableView.separatorColor = ChatMessageCell.orangeishColor
+        tableView.backgroundColor = ChatMessageCell.lightBrownishColor
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
+        cell.textLabel?.text = "placeholder"
+        return cell
+    }
+    
+    func setupNavigationItemsAndBarButtons() {
         navigationController?.navigationBar.barTintColor = ChatMessageCell.orangeishColor
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Compose, target: self, action: #selector(handleAddPostToFeed))
         navigationItem.leftBarButtonItem?.tintColor = ChatMessageCell.blackishColor
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Trash, target: self, action: #selector(handleDeleteFromFeed))
         navigationItem.rightBarButtonItem?.tintColor = ChatMessageCell.blackishColor
-        navigationItem.title = "Feed"
         
         if let font = UIFont(name: "Avenir-Heavy", size: 18) {
             navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: font]
         }
-     
-        tableView.separatorColor = ChatMessageCell.orangeishColor
-        tableView.backgroundColor = ChatMessageCell.lightBrownishColor
-
+    }
+    
+    func checkIfUserLoggedInAndSetUpNavbarTitle() {
+        if (FIRAuth.auth()?.currentUser?.uid == nil) {
+            performSelector(#selector(handleLogOut), withObject: nil, afterDelay: 0)
+        
+        } else {
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            FIRDatabase.database().reference().child("users").child(uid!).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+                if let dictionary = snapshot.value as? [String:AnyObject] {
+                    
+                    if let FeedName = dictionary["name"] as? String {
+                        self.navigationItem.title = "\(FeedName) - Feed"
+                    }
+                }
+                
+                }, withCancelBlock: nil)
+        }
+    }
+    
+    func handleLogOut() {
+        do {
+            try FIRAuth.auth()?.signOut()
+        } catch let logOutError {
+            print(logOutError)
+        }
+        
+        let loginController = LoginController()
+        presentViewController(loginController, animated: true, completion: nil)
     }
     
     func handleDeleteFromFeed() {
--- AppNormal/NewPostController.swift
+++ AppNormal/NewPostController.swift
@@ -7,16 +7,114 @@
 //
 
 import UIKit
+import Firebase
 
 class NewPostController: UIViewController {
+    
+    let instructionLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Click box below to add photo"
+        return label
+    }()
+    
+    lazy var uploadImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .ScaleAspectFill
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 8
+        image.layer.borderWidth = 3
+        image.layer.borderColor = UIColor.whiteColor().CGColor
+        image.userInteractionEnabled = true
+        image.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleGetImage)))
+        return image
+        
+    }()
+    
+    let descriptionTextField: UITextField = {
+        let txtField = UITextField()
+        txtField.placeholder = "Add message ..."
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        txtField.borderStyle = .RoundedRect
+        
+        return txtField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: #selector(handleCancel))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Save, target: self, action: #selector(handleSavePost))
+        
+        
+        
         navigationItem.leftBarButtonItem?.tintColor = ChatMessageCell.blackishColor
+        navigationItem.rightBarButtonItem?.tintColor = ChatMessageCell.blackishColor
+        
         navigationController?.navigationBar.barTintColor = ChatMessageCell.orangeishColor
        
+        view.backgroundColor = ChatMessageCell.redishColor
+        
+        setUpContainerView()
+
+       
     }
+    
+    func handleSavePost() {
+        let ref = FIRDatabase.database().reference().child("feed")
+        let childRef = ref.childByAutoId()
+        let fromId = FIRAuth.auth()!.currentUser!.uid
+        let timestamp: NSNumber = Int(NSDate().timeIntervalSince1970)
+        guard let comment = self.descriptionTextField.text else {
+            return
+        }
+        let values:[String:AnyObject] = ["fromId": fromId, "timestamp": timestamp, "comment": comment]
+        
+        childRef.updateChildValues(values) { (error, ref) in
+            if (error != nil) {
+                print(error)
+                return
+            }
+            print("Data Saved successfully")
+            
+        }
+        
+    }
+    
+    
+    
+    
+    func handleGetImage() {
+        print("Get Image")
+    }
+    
+    
+    
+    
+    func setUpContainerView() {
+        view.addSubview(descriptionTextField)
+        view.addSubview(uploadImage)
+        view.addSubview(instructionLabel)
+        
+        instructionLabel.leftAnchor.constraintEqualToAnchor(view.leftAnchor, constant: 10).active = true
+        instructionLabel.bottomAnchor.constraintEqualToAnchor(uploadImage.topAnchor, constant: 10).active = true
+        instructionLabel.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -10).active = true
+        instructionLabel.heightAnchor.constraintEqualToConstant(50).active = true
+        
+        uploadImage.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        uploadImage.bottomAnchor.constraintEqualToAnchor(descriptionTextField.topAnchor, constant: -10).active = true
+        uploadImage.heightAnchor.constraintEqualToConstant(175).active = true
+        uploadImage.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -10).active = true
+        
+        
+        descriptionTextField.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        descriptionTextField.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor, constant: -10).active = true
+        descriptionTextField.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -10).active = true
+        descriptionTextField.heightAnchor.constraintEqualToConstant(50).active = true
+        
+    }
+    
     func handleCancel() {
         dismissViewControllerAnimated(true, completion: nil)
     }
