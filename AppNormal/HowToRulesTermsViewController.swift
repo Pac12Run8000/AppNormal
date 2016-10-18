c6c52a80e53871ae491e4f2d1f3d967d84078ce2
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2016-10-18 06:34:56 -0700

    version 00.20

--- AppNormal/HowToRulesTermsViewController.swift
+++ AppNormal/HowToRulesTermsViewController.swift
@@ -9,26 +9,7 @@
 import UIKit
 
 class HowToRulesTermsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationController?.navigationBar.barTintColor = ChatMessageCell.orangeishColor
-        view.backgroundColor = ChatMessageCell.browishColor
-        navigationItem.title = "Terms of Use"
-        
-        setUpSegmentedControl()
-    }
     
-    func setUpSegmentedControl() {
-        view.addSubview(ruleSegmentController)
-        
-        ruleSegmentController.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        ruleSegmentController.topAnchor.constraintEqualToAnchor(view.topAnchor, constant: 80).active = true
-        ruleSegmentController.heightAnchor.constraintEqualToConstant(30).active = true
-        ruleSegmentController.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -20).active = true
-        
-    }
-
     lazy var ruleSegmentController: UISegmentedControl = {
         let control = UISegmentedControl(items: ["Rules","Termes of Service"])
         control.translatesAutoresizingMaskIntoConstraints = false
@@ -39,8 +20,45 @@ class HowToRulesTermsViewController: UIViewController {
         return control
     }()
     
-    func handleSelectionChanged() {
+    let contentContainerView:UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        //view.backgroundColor = UIColor.whiteColor()
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = ChatMessageCell.orangeishColor
+        view.backgroundColor = ChatMessageCell.browishColor
+        navigationItem.title = "Terms of Use"
+        
+        setUpSegmentedControl()
+        setUpContainerView()
+    }
     
+    func setUpContainerView() {
+        view.addSubview(contentContainerView)
+        
+        contentContainerView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        contentContainerView.topAnchor.constraintEqualToAnchor(ruleSegmentController.bottomAnchor, constant: 20).active = true
+        contentContainerView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor, constant: -65).active = true
+        contentContainerView.widthAnchor.constraintEqualToAnchor(view.widthAnchor).active = true
+    }
+    
+    func setUpSegmentedControl() {
+        view.addSubview(ruleSegmentController)
+        
+        ruleSegmentController.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        ruleSegmentController.topAnchor.constraintEqualToAnchor(view.topAnchor, constant: 80).active = true
+        ruleSegmentController.heightAnchor.constraintEqualToConstant(30).active = true
+        ruleSegmentController.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -20).active = true
+    }
+
+    
+    
+    func handleSelectionChanged() {
+        print(ruleSegmentController.selectedSegmentIndex)
     }
 
 }
