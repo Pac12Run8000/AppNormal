cafe391e97869a70c707226a500df943e062af42
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2016-10-20 20:12:20 -0700

    version 00.23

--- AppNormal/FeedViewController.swift
+++ AppNormal/FeedViewController.swift
@@ -14,6 +14,7 @@ class FeedViewController: UITableViewController {
     let cellId = "cellId"
     
     var posts = [Post]()
+    var users = [User]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +25,11 @@ class FeedViewController: UITableViewController {
         
         tableView.separatorColor = ChatMessageCell.orangeishColor
         tableView.backgroundColor = ChatMessageCell.lightBrownishColor
-        fetchUser()
+        fetchPost()
         
     }
     
-    func fetchUser() {
+    func fetchPost() {
         FIRDatabase.database().reference().child("feed").observeEventType(.ChildAdded, withBlock: { (snapshot) in
             
             if let dictionary = snapshot.value as? [String: AnyObject] {
@@ -48,11 +49,25 @@ class FeedViewController: UITableViewController {
         return posts.count
     }
     
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 275
+    }
+    
+    
+   
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath)
+       
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath) as! PostCell
         let post = posts[indexPath.row]
-        cell.textLabel?.text = post.comment
-        cell.detailTextLabel?.text = post.fromId
+        cell.commentLabel.text = post.comment
+        
+//        cell.detailTextLabel?.text = post.fromId
+
+
+        
+        if let postImageUrl = post.postImageUrl {
+            cell.postImageView.loadImageUsingCacheWithUrlString(postImageUrl)
+        }
         return cell
     }
     
@@ -118,9 +133,88 @@ class FeedViewController: UITableViewController {
 
 }
 
+class PaddingLabel: UILabel {
+    override func drawTextInRect(rect: CGRect) {
+        super.drawTextInRect(CGRect(x: 10, y: 20, width: 300, height: 0))
+    }
+}
+
 class PostCell: UITableViewCell {
+    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        
+//        textLabel?.frame = CGRectMake(85, textLabel!.frame.origin.y + 100, textLabel!.frame.width, textLabel!.frame.height)
+//        detailTextLabel?.frame = CGRectMake(85, detailTextLabel!.frame.origin.y + 100, detailTextLabel!.frame.width, detailTextLabel!.frame.height)
+//    }
+    
+  
+    
+    let commentLabel: PaddingLabel = {
+        var label = PaddingLabel()
+        label.backgroundColor = ChatMessageCell.orangeishColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 7
+        label.layer.borderWidth = 2
+        label.layer.backgroundColor = ChatMessageCell.blackishColor.CGColor
+        //label.numberOfLines = 5
+        
+        return label
+    
+    }()
+    
+    let userIcon: UIImageView = {
+        let imgVw = UIImageView()
+        imgVw.translatesAutoresizingMaskIntoConstraints = false
+        imgVw.image = UIImage(named: "default")
+        imgVw.layer.masksToBounds = true
+        imgVw.layer.cornerRadius = 20
+        imgVw.layer.borderWidth = 2
+        imgVw.layer.borderColor = ChatMessageCell.blackishColor.CGColor
+        return imgVw
+    }()
+    
+    let postImageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.image = UIImage(named: "default")
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        imgView.layer.masksToBounds = true
+        imgView.layer.cornerRadius = 9
+        imgView.contentMode = .ScaleAspectFill
+        return imgView
+    }()
+    
+    let padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .Subtitle, reuseIdentifier: reuseIdentifier)
+        
+        
+        addSubview(postImageView)
+        
+        addSubview(commentLabel)
+        
+        addSubview(userIcon)
+        
+        commentLabel.leftAnchor.constraintEqualToAnchor(postImageView.leftAnchor).active = true
+        commentLabel.topAnchor.constraintEqualToAnchor(postImageView.bottomAnchor, constant: 20).active = true
+        commentLabel.widthAnchor.constraintEqualToAnchor(postImageView.widthAnchor).active = true
+        commentLabel.heightAnchor.constraintEqualToConstant(40).active = true
+        
+        
+        
+        userIcon.topAnchor.constraintEqualToAnchor(postImageView.bottomAnchor, constant: -10).active = true
+        userIcon.leftAnchor.constraintEqualToAnchor(postImageView.leftAnchor).active = true
+        userIcon.widthAnchor.constraintEqualToConstant(40).active = true
+        userIcon.heightAnchor.constraintEqualToConstant(40).active = true
+        
+        postImageView.leftAnchor.constraintEqualToAnchor(self.leftAnchor, constant: 20).active = true
+        postImageView.centerYAnchor.constraintEqualToAnchor(self.centerYAnchor, constant: -30).active = true
+        postImageView.widthAnchor.constraintEqualToAnchor(self.widthAnchor, constant: -40).active = true
+        postImageView.heightAnchor.constraintEqualToConstant(200).active = true
+        
+
     }
     
     required init?(coder aDecoder: NSCoder) {
