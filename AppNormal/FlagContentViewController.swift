079919839e64c79966b187bff51568622da4cb58
Pac12Run8000 <norbertgrover@michelles-macbook-pro.local>
2016-11-15 04:27:17 -0800

    version 00.55

--- AppNormal/FlagContentViewController.swift
+++ AppNormal/FlagContentViewController.swift
@@ -41,7 +41,7 @@ class FlagContentViewController: UIViewController {
     let captionImage:UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .ScaleToFill
+        imageView.contentMode = .ScaleAspectFit
         imageView.image = UIImage(named: "default")
         imageView.backgroundColor = UIColor.lightGrayColor()
         imageView.layer.masksToBounds = true
@@ -50,6 +50,55 @@ class FlagContentViewController: UIViewController {
         imageView.layer.borderColor = ChatMessageCell.redishColor.CGColor
         return imageView
     }()
+    
+    let dateLabel:PaddingLabel = {
+        let label = PaddingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
+        label.layer.borderColor = ChatMessageCell.redishColor.CGColor
+        label.layer.borderWidth = 3
+        label.backgroundColor = ChatMessageCell.orangeishColor
+        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+        return label
+    }()
+    
+    let directionLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+//        label.backgroundColor = ChatMessageCell.lightBrownishColor
+        label.numberOfLines = 15
+        label.textColor = UIColor.whiteColor()
+        
+        label.text = "Make sure that you are appropriately flagging a users content. If you are not sure as to whether content should be flagged then check the Terms Of Use section. Flagging content can get users terminated and/or permanently banned."
+        return label
+    }()
+    
+    let complaintTextField:UITextView = {
+        let textField = UITextView()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12)
+        textField.backgroundColor = ChatMessageCell.lightBrownishColor
+        textField.layer.cornerRadius = 7
+        textField.textColor = UIColor.whiteColor()
+        textField.contentInset = UIEdgeInsetsMake(-7.0,0.0,0,0.0)
+        return textField
+    }()
+    
+    
+    
+    func getDateFormat(timestamp:NSNumber) -> String {
+        
+        let seconds = timestamp.doubleValue
+        let timeStampDate = NSDate(timeIntervalSince1970: seconds)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss a"
+        let timeVal = dateFormatter.stringFromDate(timeStampDate)
+        
+        return timeVal
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -82,6 +131,10 @@ class FlagContentViewController: UIViewController {
         if let videoUrl = post?.videoUrl, url = NSURL(string: videoUrl) {
             self.captionImage.image = generateThumnail(url, fromTime: Float64(1.22))
         }
+        if let timestamp = post?.timestamp {
+            self.dateLabel.text = "Date: " + getDateFormat(timestamp)
+        }
+        
     }
     
     func generateThumnail(url : NSURL, fromTime:Float64) -> UIImage? {
@@ -106,6 +159,24 @@ class FlagContentViewController: UIViewController {
         view.addSubview(nameLabel)
         view.addSubview(profileImageView)
         view.addSubview(captionImage)
+        view.addSubview(dateLabel)
+        view.addSubview(directionLabel)
+        view.addSubview(complaintTextField)
+        
+        complaintTextField.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        complaintTextField.topAnchor.constraintEqualToAnchor(directionLabel.bottomAnchor, constant: 10).active = true
+        complaintTextField.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -10).active = true
+        complaintTextField.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor, constant: -10).active = true
+        
+        directionLabel.topAnchor.constraintEqualToAnchor(captionImage.bottomAnchor, constant: 10).active = true
+        directionLabel.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+        directionLabel.widthAnchor.constraintEqualToAnchor(view.widthAnchor, constant: -20).active = true
+        directionLabel.heightAnchor.constraintEqualToConstant(70).active = true
+        
+        dateLabel.bottomAnchor.constraintEqualToAnchor(captionImage.topAnchor, constant: -10).active = true
+        dateLabel.centerXAnchor.constraintEqualToAnchor(captionImage.centerXAnchor).active = true
+        dateLabel.widthAnchor.constraintEqualToAnchor(captionImage.widthAnchor).active = true
+        dateLabel.heightAnchor.constraintEqualToConstant(30).active = true
         
         
         captionImage.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
